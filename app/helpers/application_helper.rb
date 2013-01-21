be0c02ca703576acef46f122a8dcbf7efe78d73a
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-01-21 06:22:07 +0100

    fixed problem with nested resources

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Public: Pick the correct arguments for form_for when shallow routes
+  # are used.
+  #
+  # parent - The Resource that has_* child
+  # child - The Resource that belongs_to parent.
+  def shallow_args(parent, child)
+    child.try(:new_record?) ? [parent, child] : child
+  end
+
 end
