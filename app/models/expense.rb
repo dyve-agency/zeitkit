256aaae60d765eddffbdc22a5569a65873963ed8
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-03-27 15:28:39 +0100

    remove unneeded columns

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,31 +2,20 @@ class Expense < ActiveRecord::Base
   include TotalHelper
   include NilStrings
 
-  attr_accessible :client_id, :paid, :total, :user_id, :reason
+  attr_accessible :client_id, :total, :user_id, :reason
 
   belongs_to :user
   belongs_to :client
   belongs_to :invoice
 
-  before_validation :ensure_paid_not_nil
   validates :user_id, :client_id, :total, :reason, presence: true
 
-  scope :paid, where(paid: true)
-  scope :unpaid, where(paid: false)
+  scope :paid, where(invoice_id: !nil)
+  scope :unpaid, where(invoice_id: nil)
   scope :no_invoice, where(invoice_id: nil)
 
   def string_fields_to_nil
     [:reason]
-  end
-
-  def toggle_paid
-    self.paid ? self.paid = false : self.paid = true
-  end
-
-  def ensure_paid_not_nil
-    self.paid = false if paid.nil?
-    # return true to avoid rollback error
-    true
   end
 
   def short_reason
@@ -37,6 +26,10 @@ class Expense < ActiveRecord::Base
 
   def title
     "#{short_reason} - #{total.to_s}#{total.currency.symbol}"
+  end
+
+  def invoiced?
+    invoice_id
   end
 
 end
