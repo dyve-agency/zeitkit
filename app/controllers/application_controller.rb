28f4d4d52e181511c1e1be1461d98ffac36c45d5
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-04-11 15:20:35 +0200

    remove duplicate filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :check_and_set_mac_design
-  before_filter :check_auth_token_and_login
 
   def check_and_set_mac_design
     cookies[:mac_app] = true if params[:mac_app]
   end
 
-  def check_auth_token_and_login
-    return unless params[:access_token]
-    auto_login(AccessToken.find_by_token(params[:access_token]).user)
-  end
 end
