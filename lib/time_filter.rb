8e09dc0679a168a5ec23a8164c1210d799e00f57
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-04-12 22:04:56 +0200

    fix today bug

--- lib/time_filter.rb
+++ lib/time_filter.rb
@@ -3,7 +3,7 @@ module TimeFilter
   def self.included(base)
     base.extend(ClassMethods)
     base.class_eval do
-      scope :today, lambda {where(end_time: Time.zone.now.midnight..Time.zone.now)}
+      scope :today, lambda {where("end_time >= ?",Time.zone.now.midnight)}
       scope :this_week, lambda {where(end_time: Time.zone.now.beginning_of_week..Time.zone.now)}
       scope :this_month, lambda {where(end_time: Time.zone.now.beginning_of_month..Time.zone.now)}
       scope :older_than_this_month, lambda {where("end_time < ?", Time.zone.now.beginning_of_month)}
