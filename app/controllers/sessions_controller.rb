a78a72358723c61961e07c8db935f05b100bc6ab
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-04-14 22:27:23 +0200

    remove nesting sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = login(params[:session][:username], params[:session][:password], 1)
     if user
-      redirect_back_or_to new_user_worklog_path(current_user), :notice => "Logged in. Let's track some time."
+      redirect_back_or_to new_worklog_path, notice: "Logged in. Let's track some time."
     else
       flash.now.alert = "Email or password was invalid. <a href='#{new_password_reset_path}'>Forgot your password?</a>".html_safe
       render :new
