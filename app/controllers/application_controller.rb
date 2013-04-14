0ce2f619bbce878b0f9f084c476000cbca80afbc
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-04-14 20:30:16 +0200

    reworked clients resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def check_and_set_mac_design
     cookies[:mac_app] = true if params[:mac_app]
   end
+
+  def set_current_user(object_with_user_attribute)
+    object_with_user_attribute.user = current_user
+  end
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,17 +1,16 @@
 class ClientsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource except: [:index, :new]
 
   respond_to :html, :json
 
   def index
-    @user = User.find(params[:user_id])
-    @clients = @clients.order("created_at DESC")
-    respond_with(@clients)
+    @clients = current_user.clients.order("created_at DESC")
+    respond_with @clients
   end
 
   def new
     @client = Client.new
-    @client.user = current_user
+    set_current_user @client
     @client.hourly_rate = "0"
     @client.currency
   end
@@ -22,14 +21,20 @@ class ClientsController < ApplicationController
   def create
     @client.user = current_user
     if @client.save
-      redirect_to user_clients_path,
-        notice: "Client was successfully created. <a href='#{new_user_worklog_path(client: @client)}'>Create the first worklog for #{@client.name}.</a>".html_safe
+      redirect_to clients_path,
+        notice: "Client was successfully created.
+          <ul>
+            <li><a href='#{new_worklog_path(client: @client)}'>Create the first worklog for #{@client.name}.</a></li>
+            <li><a href='#{new_invoice_path(client: @client)}'>Or, create an invoice.</a></li>
+            <li><a href='#{client_path(@client)}'>Or, take a note.</a></li>
+          </ul>".html_safe
     else
       render action: "new"
     end
   end
 
   def update
+    @client = current_user.clients.find(params[:id])
     if @client.update_attributes(params[:client])
       redirect_to user_clients_path(current_user), notice: 'Client was successfully updated.'
     else
@@ -39,7 +44,7 @@ class ClientsController < ApplicationController
 
   def destroy
     @client.destroy
-    redirect_to user_clients_path(current_user), notice: 'Client was successfully deleted.'
+    redirect_to clients_path, notice: 'Client was successfully deleted.'
   end
 
   def show
