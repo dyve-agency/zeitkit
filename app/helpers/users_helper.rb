d46e55f59f89b910357d36ac6230d4a258c3d01f
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2013-04-18 20:33:23 +0200

    fix demo user issue

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
+
   def self_destruct_counter
-    return unless current_user
+    return unless current_user.demo?
     destruct = current_user.self_destruct_in
     "#{destruct[:hours]} hour(s), #{destruct[:minutes]} minute(s) and #{destruct[:seconds]} second(s)"
   end
