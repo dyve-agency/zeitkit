e0e3555b5ac6528a7e0c049c32d3193dc8bacb52
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2014-05-10 22:48:37 +0200

    share client only once per user

--- app/models/client.rb
+++ app/models/client.rb
@@ -24,6 +24,8 @@ class Client < ActiveRecord::Base
   validates :name, uniqueness: {scope: :user_id, message: "You can only have the client once."}
   validates_numericality_of :hourly_rate, :greater_than => 0, :allow_blank => false
 
+  validate :only_one_client_share_per_user
+
   def string_fields_to_nil
     [:company_name, :zip, :street, :city]
   end
@@ -34,5 +36,14 @@ class Client < ActiveRecord::Base
 
   def self.updated_since(unixtimestamp)
     self.unscoped.where("updated_at >= ?", Time.at(unixtimestamp.to_i).to_datetime)
+  end
+
+  def only_one_client_share_per_user
+    shares_to_check = client_shares.reject {|cs| cs.marked_for_destruction? }
+    grouped = shares_to_check.group_by {|cs| cs.user_id}
+    if grouped.select {|user_id, client_shares| client_shares.length > 1 }.present?
+      errors.add(:base, "You can only share the client once per user" )
+    end
+    true
   end
 end
