1578c567908fdd67c00ac1567ce886b70380cfb4
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2015-02-08 18:08:53 +0100

    set default value

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,9 @@ class TeamsController < ApplicationController
   def show
     @team = current_user.teams.find(params[:id])
     @form = TeamAggregator.new(params[:team_aggregator])
+    if params[:team_aggregator].blank?
+      @form.specific_range = "this_month"
+    end
     @form.team = @team
     @form.aggregate
   end
