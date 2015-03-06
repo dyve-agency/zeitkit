66da2914a3d18020945e18561f130fbca1115e78
fsainz <fsainz@gmail.com>
2015-03-06 08:10:21 +0100

    Add number of hours worked this day and during the week

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,6 +1,20 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+  def time_worked_today
+    worklogs.joins(:timeframes).
+      where("timeframes.started >= ?", DateTime.now.beginning_of_day).
+      uniq.
+      map(&:duration).inject(0, :+).to_f
+  end
+
+  def time_worked_this_week
+    worklogs.joins(:timeframes).
+      where("timeframes.started >= ?", DateTime.now.beginning_of_week).
+      uniq.
+      map(&:duration).inject(0, :+).to_f
+  end
+
   def time_worked_this_month
     worklogs.joins(:timeframes).
       where("timeframes.started >= ?", DateTime.now.beginning_of_month).
@@ -13,6 +27,14 @@ class UserDecorator < Draper::Decorator
       where("timeframes.started >= ? AND ended <= ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month).
       uniq.
       map(&:duration).inject(0, :+).to_f
+  end
+
+  def hours_today
+    h.number_with_precision (time_worked_today / 3600), precision: 2
+  end
+
+  def hours_this_week
+    h.number_with_precision (time_worked_this_week / 3600), precision: 2
   end
 
   def hours_this_month
