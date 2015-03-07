63365eceef1245e9f42be4cd5824353fcc1ea4ff
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2015-03-07 18:01:39 +0100

    cleanup columns

--- app/decorators/worklog_decorator.rb
+++ app/decorators/worklog_decorator.rb
@@ -2,4 +2,24 @@ class WorklogDecorator < Draper::Decorator
   decorates_association :timeframes
 
   delegate_all
+
+  def duration_hours_minutes
+    "#{duration_hours}:#{duration_minutes}"
+  end
+
+  def duration_hours
+    if object.duration_hours.to_s.length > 1
+      object.duration_hours
+    else
+      "0#{object.duration_hours}"
+    end
+  end
+
+  def duration_minutes
+    if object.duration_minutes.to_s.length > 1
+      object.duration_minutes
+    else
+      "0#{object.duration_minutes}"
+    end
+  end
 end
