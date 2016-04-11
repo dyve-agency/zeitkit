686532f9c8c207ebe91bb25990c23bc9f90b6fd4
philippmeissner <pm@zeit.io>
2016-04-11 19:31:27 +0200

    updated view to order for invoice_date

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,8 +5,8 @@ class InvoicesController < ApplicationController
   respond_to :html, :json
 
   def index
-    @invoices = @invoices.order("created_at DESC")
-    @chart_data = current_user.invoices.where("created_at >= ?", 1.year.ago).group_by_month(:created_at).sum("total_cents / 100")
+    @invoices = @invoices.order("invoice_date DESC")
+    @chart_data = current_user.invoices.where("invoice_date >= ?", 1.year.ago).group_by_month(:invoice_date).sum("total_cents / 100")
     respond_with @invoices
   end
 
