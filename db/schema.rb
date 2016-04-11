01991a8362b7448a3ca521dab4bd8de96551c511
philippmeissner <pm@zeit.io>
2016-04-11 19:21:20 +0200

    copied all created_at dates to new field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411171110) do
+ActiveRecord::Schema.define(version: 20160411171919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
