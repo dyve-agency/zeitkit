bf24bf7e9dd9f9036b6f997d26ceba7128b578ef
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2016-10-03 10:48:50 +0000

    fix time filters

--- lib/time_filter.rb
+++ lib/time_filter.rb
@@ -3,11 +3,11 @@ module TimeFilter
   def self.included(base)
     base.extend(ClassMethods)
     base.class_eval do
-      scope :today, lambda {where("end_time >= ?",Time.zone.now.midnight)}
-      scope :this_week, lambda {where(end_time: Time.zone.now.beginning_of_week..Time.zone.now)}
-      scope :this_month, lambda {where(end_time: Time.zone.now.beginning_of_month..Time.zone.now)}
-      scope :older_than_this_month, lambda {where("end_time < ?", Time.zone.now.beginning_of_month)}
-      scope :last_month, lambda {where(end_time: (Time.zone.now.beginning_of_month - 1.month)..Time.zone.now.beginning_of_month)}
+      scope :today, lambda {joins(:timeframes).having("MAX(timeframes.ended) >= ?", Time.zone.now.midnight).group("worklogs.id")}
+      scope :this_week, lambda {joins(:timeframes).having("MAX(timeframes.ended) BETWEEN ? AND ?", Time.zone.now.beginning_of_week, Time.zone.now).group("worklogs.id")}
+      scope :this_month, lambda {joins(:timeframes).having("MAX(timeframes.ended) BETWEEN ? AND ?", Time.zone.now.beginning_of_month, Time.zone.now).group("worklogs.id")}
+      scope :older_than_this_month, lambda {joins(:timeframes).having("MAX(timeframes.ended) < ?", Time.zone.now.beginning_of_month).group("worklogs.id")}
+      scope :last_month, lambda {joins(:timeframes).having("MAX(timeframes.ended) BETWEEN ? AND ?", (Time.zone.now.beginning_of_month - 1.month), Time.zone.now.beginning_of_month).group("worklogs.id")}
     end
   end
 
