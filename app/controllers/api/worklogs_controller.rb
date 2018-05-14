ccacca6c2780ffda693f93d6f2a2b05ead785a0c
philippmeissner <pm@zeit.io>
2018-05-14 16:16:03 +0200

    added specific error

--- app/controllers/api/worklogs_controller.rb
+++ app/controllers/api/worklogs_controller.rb
@@ -6,10 +6,10 @@ module Api
         Timeframe.create(started: timeframe[0], ended: timeframe[1]).id
       end
 
+      # WARNING: Can't mass-assign protected attributes for Worklog: client, team, timeframes
       worklog = Worklog.new(client: params[:client_id], team: params[:team_id], timeframes: timeframes)
 
-      render json: {success: worklog.save}
+      render json: { success: worklog.save! }
     end
-
   end
 end
