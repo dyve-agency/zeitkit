b75906859af3adf9eadf9fd35ea32094d4d4e530
philippmeissner <pm@zeit.io>
2018-05-14 16:27:58 +0200

    fixed timeframes

--- app/controllers/api/worklogs_controller.rb
+++ app/controllers/api/worklogs_controller.rb
@@ -3,7 +3,7 @@ module Api
 
     def create
       timeframes = params[:worklogs].map do |timeframe|
-        Timeframe.create(started: timeframe[0], ended: timeframe[1]).id
+        Timeframe.create(started: Time.at(timeframe[0]).to_datetime, ended: Time.at(timeframe[1]).to_datetime).id
       end
 
       worklog = Worklog.new(client_id: params[:client_id], team_id: params[:team_id], timeframes: timeframes)
