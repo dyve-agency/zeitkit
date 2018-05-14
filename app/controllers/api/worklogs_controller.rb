06bbe88c019f46f824fc4686881a28932e057e86
philippmeissner <pm@zeit.io>
2018-05-14 15:56:03 +0200

    added create worklog endpoint

--- app/controllers/api/worklogs_controller.rb
+++ app/controllers/api/worklogs_controller.rb
@@ -2,7 +2,13 @@ module Api
   class WorklogsController < BaseController
 
     def create
-      render json: {success: true}
+      timeframes = params[:worklogs].map do |timeframe|
+        Timeframe.create(started: timeframe[0], ended: timeframe[1]).id
+      end
+
+      worklog = Worklog.new(client: params[:client_id], team: params[:team_id], timeframes: timeframes)
+
+      render json: {success: worklog.save}
     end
 
   end
