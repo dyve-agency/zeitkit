24b6b60a9b9fc5b09284a8b00db6350766a5e7f3
philippmeissner <pm@zeit.io>
2018-05-14 16:57:07 +0200

    let's try to re-use WorkForm

--- app/controllers/api/worklogs_controller.rb
+++ app/controllers/api/worklogs_controller.rb
@@ -2,13 +2,34 @@ module Api
   class WorklogsController < BaseController
 
     def create
+      client = Client.find(params[:client_id])
+      team = Team.find(params[:team_id])
+
       timeframes = params[:worklogs].map do |timeframe|
         Timeframe.new(started: Time.at(timeframe[0]).to_datetime, ended: Time.at(timeframe[1]).to_datetime)
       end
 
-      worklog = Worklog.new(user_id: @current_user.id, client_id: params[:client_id], team_id: params[:team_id], timeframes: timeframes)
+      form = WorklogForm.new_from_params({}, user: @current_user, team: team)
 
-      render json: { success: worklog.save! }
+      form.client_id = client.id
+      form.timesframes = timeframes
+
+      if form.save
+        render json: {worklog: form.worklog}.to_json, status: 200
+      else
+        render json: {errors: form.errors.full_messages}.to_json, status: 422
+      end
+
+      # worklog = Worklog.new(
+      #   user_id: @current_user.id,
+      #   client_id: client.id,
+      #   team_id: params[:team_id],
+      #   summary: params[:description],
+      #   timeframes: timeframes,
+      #   hourly_rate_cents: client.hourly_rate
+      # )
+      #
+      # render json: { success: worklog.save! }
     end
   end
 end
