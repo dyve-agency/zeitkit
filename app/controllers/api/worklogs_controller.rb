5a62e948b6e0449121106c16719f1da8b6ea5282
philippmeissner <pm@zeit.io>
2018-05-14 16:36:17 +0200

    add missing user attribute

--- app/controllers/api/worklogs_controller.rb
+++ app/controllers/api/worklogs_controller.rb
@@ -6,7 +6,7 @@ module Api
         Timeframe.new(started: Time.at(timeframe[0]).to_datetime, ended: Time.at(timeframe[1]).to_datetime)
       end
 
-      worklog = Worklog.new(client_id: params[:client_id], team_id: params[:team_id], timeframes: timeframes)
+      worklog = Worklog.new(user_id: @current_user.id, client_id: params[:client_id], team_id: params[:team_id], timeframes: timeframes)
 
       render json: { success: worklog.save! }
     end
